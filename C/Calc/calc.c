c500a62ae747bf4a7485d6f5401258e7d7207d0a
biskwt <akioni.uwu@gmail.com>
2022-10-25 18:10:09 +0200

    Log/Register in Cpp by @akionii

---
+++ C/Calc/calc.c
@@ -0,0 +1,45 @@
+/*
+ ============================================================================
+ Name        : calc.c
+ Author      : Leonardo Acchioni Mena
+ Version     : 1.0
+ Copyright   : N/A
+ Description : Calculator in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+
+  char op;
+  double first, second;
+  printf("Enter an operator (+, -, *, /): ");
+  scanf("%c", &op);
+  printf("Enter two operands: ");
+  scanf("%lf %lf", &first, &second);
+
+  switch (op) {
+    case '+':
+      printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+      printf("/n");
+      break;
+    case '-':
+      printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+      printf("\n");
+      break;
+    case '*':
+      printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+      printf("\n");
+      break;
+    case '/':
+      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+      printf("\n");
+      break;
+    // operator doesn't match any case constant
+    default:
+      printf("Error! operator is not correct");
+  }
+
+  return 0;
+}
