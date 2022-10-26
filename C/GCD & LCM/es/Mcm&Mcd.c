0d21235e24030b5039c7d82cf915b1eb01b3a97f
biskwt <akioni.uwu@gmail.com>
2022-10-26 16:54:47 +0200

    'GCD & LCM' by @akionii

---
+++ C/GCD & LCM/es/Mcm&Mcd.c
@@ -0,0 +1,81 @@
+ /*
+ ============================================================================
+ Nombre      : Mcm&Mcd.c
+ Autor       : Leonardo Acchioni Mena
+ Descripción : Minimo comun multiplo y maximo comun divisor en C
+ ============================================================================
+ */
+
+#include <stdio.h>
+ 
+int main() {
+    /* Variables enteras definidas por el usuario 
+    sobre las que se fundamentaran las oparaciones 
+    necesarias para hallar el mcd y mcm */
+    int num1, num2;
+    num1 = 0;
+    num2 = 0;
+    
+    /* Variables enteras en las que se salvaran 
+    los valores resultantes (Minimo ..., Maximo ...)*/
+    int mcd, mcm;
+    mcd = 0;
+    mcm = 0;
+    
+    /* Variables usadas durante el algoritmo para salvar 
+    el resto, numerador y denominador */
+    int resto, numerador, denominador;
+    resto = 0;
+    numerador = 0;
+    denominador = 0;
+    
+    // Entrada de datos de las variables num1 y num2
+    printf(" Intrododuzca un primer numero entero: ");
+    scanf("%d", &num1);
+    printf(" Intrododuzca un segundo numero entero: ");
+    scanf("%d", &num2);
+    
+    /* [Operaicones] Algoritmo necesario para hallar el minimimo comun multiplo 
+    y el maximo comun divisor */
+    numerador = (num1>num2)?num1:num2;
+    denominador = (num1<num2)?num1:num2;
+    resto = numerador % denominador;
+    
+    /* Mientras el resto no se igual a cero se actualizará el valor del denominador.
+    Haciendo la división entera entre el numerador y el denominador */
+    while (resto != 0) {
+        numerador   = denominador;
+        denominador = resto;
+        resto = numerador % denominador;
+    }
+    
+    // Salvando los valores resultantes en sus variables correspondientes
+    mcd = denominador;
+    mcm = num1 * num2 / mcd;
+    
+    // Mostrando por pantalla los resultados 'mcd' y 'mcm'
+    printf("\n El M.C.D de %d y %d es: %d\n", num1, num2, mcd);
+    printf(" El M.C.M de %d y %d es: %d\n", num1, num2, mcm);
+    
+    /* Preguntandole al usuario si desea reiniciar o finalizar el programa */
+    
+    // Creando variable para salvar la respuesta de usuario
+    int otraVez = 0;
+    
+    // Opción 1.
+    printf("\n\n 1. Finalizar");
+    // Opción 2.
+    printf("\n 2. Otra vez");
+    
+    // Entrada del usuario
+    printf("\n Su elección: ");
+    scanf("%d", &otraVez);
+    
+    if(otraVez == 1) {
+        // Finaliza Programa
+    }
+    else if(otraVez == 2) {
+        printf("\n ----------------------------- Reiniciando Programa  ----------------------------- \n");
+        main(); // Vuelve al inicio de la función main()
+    }
+}
