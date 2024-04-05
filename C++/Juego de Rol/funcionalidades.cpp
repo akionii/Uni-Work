a251f8f3ee599e53f89f6555f3cabb3696ceee20
Mnco223 <felipemaestro900@gmail.com>
2024-04-05 13:54:06 +0200

    añadir clases

---
+++ C++/Juego de Rol/funcionalidades.cpp
@@ -0,0 +1,10 @@
+#include<string>
+#include<iostream>
+#include"main.h"
+
+jugador::jugador(){
+
+}
+void jugador::getNombre(){
+
+}
---
+++ C++/Juego de Rol/main.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+#include<iostream>
+using namespace std;
+
+class jugador{
+    private:
+        class stats;
+        float base_vida;
+        float base_atk;
+    public:
+        jugador();
+        void getNombre();
+        float getVida();
+};
+
+class partida{
+    private:
+    class jugador;
+    public:
+    ;
+};
+
+class stats{
+    private:
+        string nombre;
+        float vida;
+        float atk;
+
+    public:
+
+};
