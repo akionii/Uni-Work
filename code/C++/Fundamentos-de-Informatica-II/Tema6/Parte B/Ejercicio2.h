b711f4faf74f2e48c52d5fe099f239a3b929d412
Cyegua <chaosayaki@gmail.com>
2024-05-03 12:42:42 +0200

    pendejo

--- code/C++/Fundamentos-de-Informatica-II/Tema6/Parte B/Ejercicio2.h
+++ code/C++/Fundamentos-de-Informatica-II/Tema6/Parte B/Ejercicio2.h
@@ -3,5 +3,10 @@
 using namespace std;
 
 class Vector2D {
+    public:
+    float x;
+    float y;
 
+    Vector2D(): x(0), y(0) {}
+    Vector2D(float valorX, float valorY): x(valorX), y(valorY) {};
 };
